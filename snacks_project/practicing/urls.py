58196e3738ef30db0cbe1e7ad97bf836fe80e9c4
emad-almajdalawi <majdalawi97@yahoo.com>
2022-05-10 15:04:50 +0300

    remove unneeded files

--- snacks_project/practicing/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PracticingView
-
-urlpatterns = [
-    path ('practice/', PracticingView.as_view(), name='practice'),
-]
--- snacks_project/practicing/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-class PracticingView (TemplateView):
-    template_name = 'practicing.html'
